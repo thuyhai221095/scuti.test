971fdc94bd94b75c7caa5975e4c4d516167bdc75
thuyhai221095 <thuylee221095@gmail.com>
2019-05-23 14:25:47 +0700

    fix form select

--- resources/views/custom/form_select2.blade.php
+++ resources/views/custom/form_select2.blade.php
@@ -1,4 +1,4 @@
-<div class="form-group" style="width: 100%">
+<div style="width: 100%">
 	<select class="select2 form-control"  onchange="{{ @$onchange }}"  style="width: 100%" name="{{ $name }}">
  	    <option value="" >All</option>
  	    @if(isset($items) && $items != '')

832fd809e9d44c1edd36e58dd32b1b8fcdf499ac
thuyhai221095 <thuylee221095@gmail.com>
2019-05-27 11:41:42 +0700

    fix

--- resources/views/layouts/partials/htmlheader.blade.php
+++ resources/views/layouts/partials/htmlheader.blade.php
@@ -6,6 +6,8 @@
 
 <meta name="author" content="">
 
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
 <link rel="stylesheet" href="{{url('bower_components/air-datepicker/dist/css/datepicker.min.css')}}">
